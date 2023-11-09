ec14f344558330d3191947390b50b610c302a959
Samantha Symes <samantha.symes@outlook.com>
2023-11-09 11:06:19 -0330

    elecated state

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,24 @@ import ChecklistCard from "./components/checklistCard";
 import AddDocument from "./components/AddDocument";
 import Select from "./components/select/select";
 import { useState } from "react";
+import canada from "./images/canada.png";
+import usa from "./images/usa.png";
+import mexico from "./images/mexico.png";
 
 import "./App.css";
 
 function App() {
-  const [selectedCountry, setSelectedCountry] = useState("canada");
+  const [selectedCountry, setSelectedCountry] = useState(0);
+
+  const handleCountryChange = (event) => {
+    setSelectedCountry(event.target.value);
+  };
+
+  const countries = [
+    { label: "Canada", value: "canada", image: canada },
+    { label: "USA", value: "usa", image: usa },
+    { label: "Mexico", value: "mexico", image: mexico },
+  ];
 
   return (
     <>
@@ -18,16 +31,21 @@ function App() {
         <Header />
         <Select
           value={selectedCountry}
-          onChange={(event) => setSelectedCountry(event.target.value)}
+          onChange={handleCountryChange}
+          options={countries}
         />
+        <img
+          src={countries[selectedCountry].image}
+          alt={countries[selectedCountry].label}
+        />
+        ;
       </div>
       <div className="row">
-        <CountryCard country={selectedCountry} />
+        <CountryCard country={countries[selectedCountry]} />
         <ChecklistCard />
       </div>
       <AddDocument />
     </>
   );
 }
-
 export default App;
