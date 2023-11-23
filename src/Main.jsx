ca575d697128d6a86de7d1a891c45bb8f42b03a5
Samantha Symes <samantha.symes@outlook.com>
2023-11-23 15:04:32 -0330

    starting to implament react router

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import ErrorPage from "./error-page.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import ResultsPage from "./results-page.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/results",
+        element: <ResultsPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/Results-Page.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
+export default function ResultsPage() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Header />
+      </div>
+      <div className="row">you made it to the next step!</div>
+    </>
+  );
+}
