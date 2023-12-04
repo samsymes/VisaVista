c7fc0823bc825e030273599ba3df2d71456e876e
Samantha Symes <samantha.symes@outlook.com>
2023-12-04 12:01:44 -0330

    moved class to res. for easeofuse. trying out data

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,53 @@
 import Navbar from "./Navbar";
 import { useSearchParams } from "react-router-dom";
 
+class Country {
+  constructor(name, visaRequirements, vaccineRequirements, otherDocumentation) {
+    this.name = name;
+    this.visaRequirements = visaRequirements;
+    this.vaccineRequirements = vaccineRequirements;
+    this.otherDocumentation = otherDocumentation;
+  }
+
+  getRequirementsForDestination(destination) {
+    console.log(
+      `Travel Requirements from ${this.name} to ${destination.name}:`
+    );
+    console.log(`Visa Requirements: ${destination.visaRequirements}`);
+    console.log(`Vaccine Requirements: ${destination.vaccineRequirements}`);
+    console.log(`Other Documentation: ${destination.otherDocumentation}`);
+  }
+}
+
+const canada = new Country(
+  "Canada",
+  "Varies by destination",
+  "COVID-19 vaccine recommended",
+  "Travel insurance"
+);
+const usa = new Country(
+  "USA",
+  "Varies by nationality",
+  "COVID-19 vaccine required",
+  "ESTA for some"
+);
+const germany = new Country(
+  "Germany",
+  "No visa for short stays",
+  "COVID-19 vaccine recommended",
+  "Proof of funds"
+);
+const france = new Country(
+  "France",
+  "Schengen visa for some",
+  "COVID-19 vaccine recommended",
+  "Proof of accommodation"
+);
+
+canada.getRequirementsForDestination(usa);
+canada.getRequirementsForDestination(germany);
+canada.getRequirementsForDestination(france);
+
 function Results() {
   const [searchParams] = useSearchParams();
   const From = searchParams.get("From");
