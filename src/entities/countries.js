404ce6875186dccec92365659921e322a466f99e
Samantha Symes <samantha.symes@outlook.com>
2023-12-06 15:01:59 -0330

    added methods for the funcs in visaRequServ

--- src/entities/countries.js
+++ src/entities/countries.js
@@ -6,33 +6,5 @@ class Country {
     this.otherDocumentation = otherDocumentation;
   }
 }
-const ca = new Country(
-  "Canada",
-  "Varies by destination",
-  "COVID-19 vaccine recommended",
-  "Travel insurance"
-);
-const us = new Country(
-  "USA",
-  "Varies by nationality",
-  "COVID-19 vaccine required",
-  "ESTA for some"
-);
-const de = new Country(
-  "Germany",
-  "No visa for short stays",
-  "COVID-19 vaccine recommended",
-  "Proof of funds"
-);
-const fr = new Country(
-  "France",
-  "Schengen visa for some",
-  "COVID-19 vaccine recommended",
-  "Proof of accommodation"
-);
-
-ca.getRequirementsForDestination(us);
-ca.getRequirementsForDestination(de);
-ca.getRequirementsForDestination(fr);
 
 export default Country;
