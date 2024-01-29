548f458a1dff9ada148b8f2fc95b822737feae4c
Samantha Symes <samantha.symes@outlook.com>
2024-01-29 11:35:05 -0330

    added class component for educational purposes

--- src/components/buttons/SocialsButton.jsx
+++ src/components/buttons/SocialsButton.jsx
@@ -1,18 +1,36 @@
 import IconButton from "@mui/material/IconButton";
+import { Component } from "react";
 import { PropTypes } from "prop-types";
-function SocialsButton(props) {
-  const button = (
-    <IconButton
-      href={props.href}
-      color={props.color}
-      aria-label={props.ariaLabel}
-    >
-      {props.iconElement}
-    </IconButton>
-  );
-  return button;
+
+class SocialsButton extends Component {
+  state = {
+    href: this.props.href,
+    color: this.props.color,
+    ariaLabel: this.props.ariaLabel,
+    iconElement: this.props.iconElement,
+  };
+
+  componentDidMount() {
+    console.log("SocialsButton mounted");
+  }
+  componentDidUpdate() {
+    console.log("SocialsButton updated");
+  }
+  componentWillUnmount() {
+    console.log("SocialsButton unmounted");
+  }
+  render() {
+    return (
+      <IconButton
+        href={this.props.href}
+        color={this.props.color}
+        aria-label={this.props.ariaLabel}
+      >
+        {this.props.iconElement}
+      </IconButton>
+    );
+  }
 }
-export default SocialsButton;
 
 SocialsButton.propTypes = {
   href: PropTypes.string,
@@ -20,3 +38,17 @@ SocialsButton.propTypes = {
   ariaLabel: PropTypes.string,
   iconElement: PropTypes.element,
 };
+
+// function SocialsButton(props) {
+//   const button = (
+//     <IconButton
+//       href={props.href}
+//       color={props.color}
+//       aria-label={props.ariaLabel}
+//     >
+//       {props.iconElement}
+//     </IconButton>
+//   );
+//   return button;
+// }
+export default SocialsButton;
