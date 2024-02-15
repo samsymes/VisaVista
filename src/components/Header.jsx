fe95af7dc6bfcbdbbc7815d9a981602472c297fc
Samantha Symes <samantha.symes@outlook.com>
2024-02-15 12:33:16 -0330

    styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,7 @@
-import Navbar from "./Navbar";
-
 function Header() {
   return (
     <>
       <div className="header">
-        <Navbar />
         <div className="headerText">
           <p>From A to B. Hassel Free...</p>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,65 +4,44 @@ import Github from "@mui/icons-material/GitHub";
 import LinkedIn from "@mui/icons-material/LinkedIn";
 import Email from "@mui/icons-material/Email";
 import Logo from "../assets/Logo";
+
 function Navbar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light">
+      <nav className="navbar">
         <Logo id="logo" />
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="#about">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#projects">
-                Projects
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-          <form className="d-flex">
-            <Stack direction="row" spacing={1}>
-              <SocialsButton
-                href="https://github.com/samsymes"
-                color="default"
-                ariaLabel="GitHub"
-                iconElement={<Github />}
-              ></SocialsButton>
-              <SocialsButton
-                href="https://www.linkedin.com/in/samanthasymes/"
-                color="default"
-                ariaLabel="LinkedIn"
-                iconElement={<LinkedIn />}
-              ></SocialsButton>
-              <SocialsButton
-                href="mailto:samasymes@gmail.com"
-                color="default"
-                ariaLabel="Email"
-                iconElement={<Email />}
-              ></SocialsButton>
-            </Stack>
-          </form>
-        </div>
+        <form className="d-flex">
+          <Stack direction="row" spacing={1}>
+            <a className="nav-link" href="#about">
+              About
+            </a>
+            <a className="nav-link" href="#projects">
+              Projects
+            </a>
+            <a className="nav-link" href="#contact">
+              Contact
+            </a>
+            <SocialsButton
+              href="https://github.com/samsymes"
+              color="default"
+              ariaLabel="GitHub"
+              iconElement={<Github />}
+            ></SocialsButton>
+            <SocialsButton
+              href="https://www.linkedin.com/in/samanthasymes/"
+              color="default"
+              ariaLabel="LinkedIn"
+              iconElement={<LinkedIn />}
+            ></SocialsButton>
+            <SocialsButton
+              href="mailto:samasymes@gmail.com"
+              color="default"
+              ariaLabel="Email"
+              iconElement={<Email />}
+            ></SocialsButton>
+          </Stack>
+        </form>
       </nav>
     </>
   );
