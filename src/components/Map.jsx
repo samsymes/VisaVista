c4cf29d6956cf79fd9649a7df1e559f1cb0477ea
Sam Symes <samasymes@gmail.com>
2024-03-15 16:11:59 -0230

    falcon?

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -28,6 +28,7 @@ function Map(props) {
   Ion.defaultAccessToken =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzZDhiNmY0ZC1kZmU3LTQ2YjQtOTNhYi0xOGY4YTQyNDI3NzQiLCJpZCI6MTg1MzkwLCJpYXQiOjE3MDMwMjI1MTd9.NyKMKAqdzoRhgLLvDxOBOkvzOQTQTWaupOA_tdyj8RM";
   const cesiumRef = useRef(null);
+  const modelUrl = "./millennium_falcon.glb";
   useEffect(() => {
     if (
       cesiumRef.current &&
@@ -94,7 +95,7 @@ function Map(props) {
         ]),
         position: sampledProp,
         model: {
-          uri: "./millennium_falcon.glb",
+          uri: modelUrl,
           minimumPixelSize: 128,
           maximumScale: 200000,
         },
