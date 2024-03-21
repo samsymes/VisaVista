2445c00422455321ad8be9c226a5549c79797a03
Sam Symes <samasymes@gmail.com>
2024-03-21 11:18:40 -0230

    404 error

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,21 +1,22 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./Routes/App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Results from "./Routes/Results.jsx";
 import Flights from "./Routes/Flights.jsx";
 import About from "./Routes/About.jsx";
 import "./main.css";
+
 const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
   <>
-    <Router>
+    <BrowserRouter baseName={import.meta.env.DEV ? "/" : "/VisaVista"}>
       <Routes>
-        <Route path="/VisaVista" index element={<App />} />
-        <Route path="/VisaVista/Results" element={<Results />} />
-        <Route path="/VisaVista/Flights" element={<Flights />} />
-        <Route path="VisaVista/About" element={<About />} />
+        <Route path="/" index element={<App />} />
+        <Route path="/Results" element={<Results />} />
+        <Route path="/Flights" element={<Flights />} />
+        <Route path="/About" element={<About />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </>
 );
