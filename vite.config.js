31386acb20798efa631197bd5c437da2bb81ac5b
Sam Symes <samasymes@gmail.com>
2024-03-21 11:08:52 -0230

    404 error

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import cesium from "vite-plugin-cesium";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), cesium()],
-  base: "/VisaVista/",
-  assetsInclude: ["**/*.glb"],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react(), cesium()],
+    base: "/",
+    assetsInclude: ["**/*.glb"],
+  };
+  if (command !== "serve") {
+    config.base = "/VisaVista/";
+  }
+
+  return config;
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import cesium from "vite-plugin-cesium";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), cesium()],
+//   base: "/VisaVista/",
+//   assetsInclude: ["**/*.glb"],
+// });
