054055d7fa0235a5f24e0652cd60270b62bc18a5
Ssymes-dev <samasymes@gmail.com>
2024-08-19 13:24:48 -0400

    search params work in postman. not browser

--- src/services/RestCountriesService.ts
+++ src/services/RestCountriesService.ts
@@ -1,24 +1,19 @@
-// possible to query for multiple countries
-// https://restcountries.com/v3.1/alpha?codes={code},{code},{code}
-
-// possible to filter the output of your request to include only the specified fields.
-// https://restcountries.com/v3.1/{service}?fields={field},{field},{field}
-// https://restcountries.com/v3.1/all?fields=name,capital,currencies
+import { useSearchParams } from "react-router-dom";
 
 class RestCountriesService {
   constructor() {}
 
   async getCountryInfo() {
     try {
-      const response = await fetch("http://localhost:3002/rest-countries");
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = response.json();
-      console.log("Country data:", data);
-      return data;
+      const [searchParams] = useSearchParams();
+      const From = searchParams.get("From");
+      const To = searchParams.get("To");
+      const response = await fetch(
+        `http://localhost:3002/rest-countries?From=${From}&To=${To}`
+      );
+      const countryInfo = await response.json();
+      console.log("Country data:", countryInfo);
+      return countryInfo;
     } catch (error) {
       console.error("Error fetching country stats:", error);
       throw error;
